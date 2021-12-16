411366c3797e9f4bd0650e02ea71f20b34bf6818
badboy <2268962023@qq.com>
2021-12-16 11:16:12 +0800

    自定义登录完成

--- src/main/java/com/badboy/config/SecurityConfig.java
+++ src/main/java/com/badboy/config/SecurityConfig.java
@@ -6,9 +6,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.annotation.Resource;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Resource
+    private AuthenticationSuccessHandler successHandler;
+
+    @Resource
+    private AuthenticationFailureHandler failureHandler;
 
     @Bean
     public PasswordEncoder getPwdEncoder() {
@@ -21,9 +31,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.formLogin()
                 .usernameParameter("username") //设置用户名和password的参数名
                 .passwordParameter("password")
-                .failureForwardUrl("/fail")//当登陆失败时，请求fail
+                //.failureForwardUrl("/fail")//当登陆失败时，请求fail
                 .loginProcessingUrl("/login")//当发现是login时，认为是登录，执行UserDetailsServiceImpl这个类。//若不写这行，将不执行
-                .successForwardUrl("/toMain")//当请求成功时，执行toMain,此处为post请求
+                //.successForwardUrl("/toMain")//当请求成功时，执行toMain,此处为post请求
+                .successHandler(successHandler)
+                .failureHandler(failureHandler)
                 .loginPage("/login.html");//登陆页面为login.html
         //url拦截
         http.authorizeRequests()
